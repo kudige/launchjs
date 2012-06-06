44de3ca2b95b204909ad19c8cd1bd34ce1c86865
Chandan Kudige <github@kudige.com>
2012-06-06 13:27:05 +1000

    Created HttpIO class for attaching controller contexts

---
+++ test/testskeleton.js
@@ -0,0 +1,53 @@
+#!/usr/bin/env node
+
+/*
+ * Use this file as a template for adding new test cases
+ */
+var Jstest = require('./jstest'),
+Util = require('../lib/utils')
+
+/* --------------- TEST ROUTINES ------------------- */
+
+var Tests = function() {
+	Jstest.call(this)
+}
+Util.inherits(Tests, Jstest)
+
+Tests.prototype.init = function() {
+	Jstest.prototype.init.call(this)
+	var self = this
+	self.assertEquals('init', 2+2, 4)
+}
+
+Tests.prototype.test1 = function() {
+	var self = this
+	self.assertEquals('test1', 3+3, 6)
+	self.next()
+}
+
+Tests.prototype.test2 = function() {
+	var self = this
+	self.assertEquals('test2', 4+4, 8)
+	self.next()
+}
+
+
+// Add pending objects to one-to-many relation of a waiting object
+
+Tests.prototype.finish =  function() {
+	Jstest.prototype.finish.call(this)
+	console.log('All finished')
+	process.exit(0)
+}
+
+var set = undefined
+if (process.argv[2] == '-i') {
+	set = 'testinit'
+}
+
+process.argv.shift()
+process.argv.shift()
+
+var tests = new Tests()
+tests.showPass = true
+tests.start(process.argv)
